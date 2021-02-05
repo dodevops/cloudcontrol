87c2705dab98725bc34e4dd4afedaefe2fb2011d
Dennis Ploeger <develop@dieploegers.de>
2021-02-05 11:29:04 +0100

    feat: Added yq

---
+++ feature/yq/install.sh
@@ -0,0 +1,9 @@
+. /feature-installer-utils.sh
+
+TEMPDIR=$(mktemp -d)
+cd "${TEMPDIR}" || exit
+execHandle 'Downloading yq binary' curl -f -s -L "https://github.com/mikefarah/yq/releases/download/v${YQ_VERSION:-4.5.0}/yq_linux_amd64" -o yq
+execHandle 'Making yq executable' chmod +x yq
+execHandle 'Installing yq' mv yq ~/bin
+cd - &>/dev/null || exit
+rm -rf "${TEMPDIR}"
