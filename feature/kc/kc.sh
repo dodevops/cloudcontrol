80d319d711ba4b5fcc0933fed2c2a3b046fc49d5
Benjamin Bauer <benjamin.bauer@kps.com>
2022-08-19 12:37:40 +0200

    fix: display correct menu message for namespace switching. Add IDEA config file to gitignore

--- feature/kc/kc.sh
+++ feature/kc/kc.sh
@@ -12,7 +12,7 @@ if [ "X${1}X" == "X-nX" ]
 then
   NAMESPACES=()
   while IFS='' read -r line; do NAMESPACES+=("$line" "$line"); done < <(kubectl get ns --no-headers=true -o name | sed -re 's/namespace\/(.*)/\1/gi')
-  if dialog --keep-tite --no-tags --default-item "$(kubectl config current-context)" --menu "Please select a k8s context" 0 0 5 "${NAMESPACES[@]}" 2>"${TEMPFILE}"
+  if dialog --keep-tite --no-tags --default-item "$(kubectl config current-context)" --menu "Please select a k8s namespace" 0 0 5 "${NAMESPACES[@]}" 2>"${TEMPFILE}"
   then
       kubectl config set-context --current --namespace="$(cat "${TEMPFILE}")"
   fi
