a5744a3a15c764393279a6397811b1861e2090b5
Dennis Ploeger <dennis.ploeger@kps.com>
2022-09-19 14:30:42 +0200

    fix: Fixes Bitwarden installation
    This fixes the Bitwarden installation to use the NPM package instead because of linker problems on arch systems

--- feature/bitwarden/install.sh
+++ feature/bitwarden/install.sh
@@ -2,14 +2,9 @@
 
 if [ "X$(cat /home/cloudcontrol/flavour)X" == "XazureX" ] || [ "X$(cat /home/cloudcontrol/flavour)X" == "XsimpleX" ] || [ "X$(cat /home/cloudcontrol/flavour)X" == "XtanzuX" ]
 then
-  execHandle "Installing required libraries" sudo apk add gcompat
+  execHandle "Installing required libraries" sudo apk add nodejs npm
 fi
 
-TEMPDIR=$(mktemp -d)
-cd "${TEMPDIR}" || exit
-execHandle "Downloading bitwarden client" curl -L -o bw.zip "https://vault.bitwarden.com/download/?app=cli&platform=linux"
-execHandle "Unzipping bitwarden client" unzip bw.zip
-execHandle "Making bitwarden client executable" chmod +x bw
-execHandle "Installing bitwarden client" mv bw /home/cloudcontrol/bin
-cd - &>/dev/null || exit
-rm -rf "${TEMPDIR}"
+cd ~ || exit 1
+execHandle "Installing bitwarden client" npm install @bitwarden/cli
+execHandle "Linking bitwarden client" ln -s /home/cloudcontrol/node_modules/.bin/bw bin/bw
