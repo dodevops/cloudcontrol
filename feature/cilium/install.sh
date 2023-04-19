1c10f405c2d9754fbbee2fd3a28b850e76367c7b
mlauke <marko.lauke@gmail.com>
2023-04-19 11:36:20 +0200

    +initial

---
+++ feature/cilium/install.sh
@@ -0,0 +1,16 @@
+. /feature-installer-utils.sh
+
+if [ -n "${CILIUM_CLI_VERSION}" ]
+then
+  CILIUM_CLI_VERSION=$(checkAndCleanVersion "${CILIUM_CLI_VERSION}")
+  echo "Using git version ${CILIUM_CLI_VERSION}..."
+else
+  CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/master/stable.txt)
+fi
+
+CLI_ARCH=$(getPlatform)
+
+sudo curl -# -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
+sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
+sudo rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
