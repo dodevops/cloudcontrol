b88f572b64c561d40289725ccc0f3ae56362599d
Dennis Ploeger <dennis.ploeger@kps.com>
2024-08-06 10:20:08 +0200

    feat: Support for Tanzucli
    Includes fixes to the feature-installer-utils that came up during development

--- feature/k9s/install.sh
+++ feature/k9s/install.sh
@@ -10,7 +10,7 @@ fi
 TEMPDIR=$(mktemp -d)
 cd "${TEMPDIR}" || exit
 
-downloadFromGithub derailed k9s "${K9S_VERSION}" k9s_Linux_ tar.gz k9s.tar.gz
+downloadFromGithub derailed k9s "${K9S_VERSION}" k9s_Linux_ .tar.gz k9s.tar.gz
 
 execHandle "Unpacking k9s" tar xzf k9s.tar.gz
 execHandle "Installing k9s" mv k9s /home/cloudcontrol/bin
