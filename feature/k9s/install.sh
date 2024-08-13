0d04e0fff54f3220ff851e52a2590330fe08f711
Dennis Ploeger <dennis.ploeger@kps.com>
2024-08-13 14:02:47 +0200

    fix: Fixes k9s
    Apparently, this has never worked correctly before.

--- feature/k9s/install.sh
+++ feature/k9s/install.sh
@@ -1,16 +1,11 @@
 . /feature-installer-utils.sh
 
-if [ -n "${K9S_VERSION}" ]
-then
-  K9S_VERSION=$(checkAndCleanVersion "${K9S_VERSION}")
-else
-  K9S_VERSION="latest"
-fi
+K9S_VERSION=$(checkAndCleanVersion "${K9S_VERSION}")
 
 TEMPDIR=$(mktemp -d)
 cd "${TEMPDIR}" || exit
 
-downloadFromGithub derailed k9s "${K9S_VERSION}" k9s_Linux_ .tar.gz k9s.tar.gz
+downloadFromGithub derailed k9s "v${K9S_VERSION}" k9s_Linux_ .tar.gz k9s.tar.gz
 
 execHandle "Unpacking k9s" tar xzf k9s.tar.gz
 execHandle "Installing k9s" mv k9s /home/cloudcontrol/bin
