2c7eb7de86264a30e62fa56fd53cc4ae1ce43fdc
Dennis Ploeger <dennis.ploeger@kps.com>
2025-04-11 10:10:41 +0200

    feat: Fixed a lot of regressions
    I updated vue and vuetify before to a new major version without fixing the breaking changes apparently.
    
    This code layout now matches with the recommended way of doing Vue and Vuetify.

--- ccc-client/src/App.vue
+++ ccc-client/src/App.vue
@@ -1,15 +1,13 @@
 <template>
   <v-app>
-    <v-main>
-      <v-container class="fill-height" fluid>
-        <v-row align="center" justify="center">
-          <v-col cols="12" sm="8" md="8">
-            <Startup v-if="status === 'INIT'"/>
-            <MainScreen v-if="status === 'INITIALIZED'"/>
-          </v-col>
-        </v-row>
-      </v-container>
-      <v-footer absolute>
+    <v-layout ref="app">
+      <v-main class="d-flex align-center justify-center">
+        <v-container>
+          <Progress v-if="status === 'INIT'"/>
+          <MainScreen v-if="status === 'INITIALIZED'"/>
+        </v-container>
+      </v-main>
+      <v-footer app>
         <v-sheet class="text-body-2 w-100">
           <v-container fluid>
             <v-row dense>
@@ -23,49 +21,35 @@
           </v-container>
         </v-sheet>
       </v-footer>
-    </v-main>
+    </v-layout>
   </v-app>
 </template>
 
-<script lang="ts">
-import Vue, { defineComponent } from 'vue';
-import Progress from '@/components/Progress.vue';
+<script setup lang="ts">
+import { onMounted, onUnmounted, Ref, ref } from 'vue';
 import * as axios from 'axios';
 import MainScreen from '@/components/MainScreen.vue';
-import logoUrl from './assets/logo.svg?url';
+import logoUrlImport from './assets/logo.svg?url';
+import Progress from '@/components/Progress.vue';
 
-let status: string = 'INIT';
-let loadStatusInterval: NodeJS.Timer | null = null;
+const status = ref('');
+const loadStatusInterval: Ref<NodeJS.Timer | null> = ref(null);
+const logoUrl = ref(logoUrlImport);
 
-export default defineComponent({
-  components: { MainScreen, Startup: Progress },
-  data() {
-    return {
-      status,
-      loadStatusInterval,
-      logoUrl,
-    };
-  },
-  created() {
-    this.loadStatusInterval = setInterval(this.loadStatus.bind(this), 3000);
-    this.loadStatus();
-  },
-  methods: {
-    beforeDestroy() {
-      if (this.loadStatusInterval) {
-        clearInterval(loadStatusInterval as NodeJS.Timer);
-      }
-      this.loadStatusInterval = null;
-    },
-    loadStatus() {
-      axios.default.get('/api/status')
-        .then(
-          (backendStatus) => {
-            this.status = backendStatus.data.status;
-          },
-        );
-    },
-  },
+async function loadStatus() {
+  const backendStatus = await axios.default.get('/api/status');
+  status.value = backendStatus.data.status;
+}
+
+onMounted(async () => {
+  loadStatusInterval.value = setInterval(loadStatus.bind(this), 3000);
+  await loadStatus();
 });
 
+onUnmounted(() => {
+  if (loadStatusInterval.value) {
+    clearInterval(loadStatusInterval.value as NodeJS.Timer);
+  }
+  loadStatusInterval.value = null;
+});
 </script>
